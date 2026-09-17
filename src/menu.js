c1085bcc4ad687d57536f0f0ae3f6e145581e734
Benddri <fatimabenddri@email.com>
2026-09-17 18:17:17 +0200

    prompt

---
+++ src/menu.js
@@ -0,0 +1,15 @@
+const prompt =require("prompt")
+const {
+normaliserNom,
+validerResultat,
+ajouterApprenant,
+enregistrerResultat,
+
+
+} = require("./fonction");
+const { apprenants }= require("./data");
+prompt.start()
+function afficherMenu(){
+
+    
+}
