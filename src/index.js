ccb4c08315c8abadc3201abf683db7151b5d34dc
Benddri <fatimabenddri@email.com>
2026-09-17 18:15:46 +0200

    import fonction

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { apprenants } from "./data.js";
+import { validerresultat } from "./fonction.js";
+import { normaliserNom } from "./fonction.js";
+import { ajouterApprenant } from "./fonction.js";
+import { enregistrerResultat } from "./fonction.js";
